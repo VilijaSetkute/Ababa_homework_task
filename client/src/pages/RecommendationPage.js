2c7caf50f9a8ee7d673b79a4a25f6b2db25af095
Vilija Mažeikė <vilija.setkute@gmail.com>
2022-06-01 12:20:04 +0300

    eslinted whole client folder

--- client/src/pages/RecommendationPage.js
+++ client/src/pages/RecommendationPage.js
@@ -1,26 +1,31 @@
-import React, {useContext} from 'react';
-import Recommendation from '../components/recommendation/Recommendation'
-import mainContext from "../context/mainContext";
-import {useNavigate} from "react-router-dom";
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Recommendation from '../components/recommendation/Recommendation';
+import mainContext from '../context/mainContext';
 
-const RecommendationPage = () => {
+function RecommendationPage() {
+  const { user } = useContext(mainContext);
+  const nav = useNavigate();
 
-    const {user} = useContext(mainContext)
-    const nav = useNavigate()
-
-    return (
-        <div>
-            {user ?
-                <Recommendation/>
-                :
-                <div className='content d-flex flex-column justify-content-center align-items-center fs-3'>
-                    <div>This content is private</div>
-                    <div>Login or register to access it</div>
-                    <button className='btn btn-outline-danger my-3 fs-4' onClick={() => nav('/')}>Gain access</button>
-                </div>
-            }
-        </div>
-    );
-};
+  return (
+    <div>
+      {user
+        ? <Recommendation />
+        : (
+          <div className="content d-flex flex-column justify-content-center align-items-center fs-3">
+            <div>This content is private</div>
+            <div>Login or register to access it</div>
+            <button
+              type="button"
+              className="btn btn-outline-danger my-3 fs-4"
+              onClick={() => nav('/')}
+            >
+              Gain access
+            </button>
+          </div>
+        )}
+    </div>
+  );
+}
 
 export default RecommendationPage;
--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -1,26 +1,31 @@
-import React, {useContext} from 'react';
-import Search from "../components/search/Search";
-import mainContext from "../context/mainContext";
-import {useNavigate} from "react-router-dom";
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Search from '../components/search/Search';
+import mainContext from '../context/mainContext';
 
-const SearchPage = () => {
+function SearchPage() {
+  const { user } = useContext(mainContext);
+  const nav = useNavigate();
 
-    const {user} = useContext(mainContext)
-    const nav = useNavigate()
-
-    return (
-        <div>
-            {user ?
-                <Search/>
-                :
-                <div className='content d-flex flex-column justify-content-center align-items-center fs-3'>
-                    <div>This content is private</div>
-                    <div>Login or register to access it</div>
-                    <button className='btn btn-outline-danger my-3 fs-4' onClick={() => nav('/')}>Gain access</button>
-                </div>
-            }
-        </div>
-    );
-};
+  return (
+    <div>
+      {user
+        ? <Search />
+        : (
+          <div className="content d-flex flex-column justify-content-center align-items-center fs-3">
+            <div>This content is private</div>
+            <div>Login or register to access it</div>
+            <button
+              type="button"
+              className="btn btn-outline-danger my-3 fs-4"
+              onClick={() => nav('/')}
+            >
+              Gain access
+            </button>
+          </div>
+        )}
+    </div>
+  );
+}
 
 export default SearchPage;
